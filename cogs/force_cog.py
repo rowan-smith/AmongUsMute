a600e4c8c952fa9fc2784bc36ec032f538258421
Rowan <rowanpsmith@yahoo.com.au>
2020-09-10 22:50:08 +1000

    Move force end game into force cog

--- cogs/force_cog.py
+++ cogs/force_cog.py
@@ -2,21 +2,23 @@ import traceback
 
 from discord import VoiceState
 from discord.ext import commands
-from discord.ext.commands import BotMissingPermissions, MissingPermissions
+from discord.ext.commands import BotMissingPermissions, MissingPermissions, BucketType, CommandInvokeError
 
 from main import AmongUs
-from utils import is_in_voice, VoiceNotConnected
+from utils import is_in_voice, VoiceNotConnected, is_playing
+from utils.utils import get_game, end_game
 
 
 class Force(commands.Cog):
     def __init__(self, bot):
         self.bot: AmongUs = bot
 
-    @commands.command(aliases=["forcemute"])
+    @commands.command(name="fmute")
     @commands.has_guild_permissions(mute_members=True)
     @commands.bot_has_guild_permissions(mute_members=True)
+    @commands.cooldown(1, 10, BucketType.channel)
     @is_in_voice()
-    async def fmute(self, ctx):
+    async def force_mute(self, ctx):
         """Force mute everyone in the voice channel."""
 
         voice: VoiceState = ctx.author.voice
@@ -26,11 +28,11 @@ class Force(commands.Cog):
 
         await ctx.send(f"**{len(voice.channel.members)}** Members in channel **{voice.channel.name}** mute.")
 
-    @commands.command(aliases=["forceunmute"])
+    @commands.command(name="funmute")
     @commands.has_guild_permissions(mute_members=True)
     @commands.bot_has_guild_permissions(mute_members=True)
     @is_in_voice()
-    async def funmute(self, ctx):
+    async def force_unmute(self, ctx):
         """Force unmute everyone in the voice channel."""
 
         voice: VoiceState = ctx.author.voice
@@ -40,11 +42,11 @@ class Force(commands.Cog):
 
         await ctx.send(f"**{len(voice.channel.members)}** Members in channel **{voice.channel.name}** unmute.")
 
-    @commands.command(aliases=["forceundeafan"])
+    @commands.command(name="fundeafen")
     @commands.has_guild_permissions(deafen_members=True)
     @commands.bot_has_guild_permissions(deafen_members=True)
     @is_in_voice()
-    async def fundeafen(self, ctx):
+    async def force_undeafen(self, ctx):
         """Force undeafen everyone in the voice channel."""
 
         voice: VoiceState = ctx.author.voice
@@ -54,9 +56,17 @@ class Force(commands.Cog):
 
         await ctx.send(f"**{len(voice.channel.members)}** Members in channel **{voice.channel.name}** undeafened.")
 
-    @fmute.error
-    @funmute.error
-    @fundeafen.error
+    @commands.command(name="fendgame")
+    @is_playing()
+    async def force_end_game(self, ctx):
+        game = await get_game(self.bot.games, ctx)
+        await end_game(self.bot.games, game)
+
+        await ctx.send("Game has ended.")
+
+    @force_mute.error
+    @force_unmute.error
+    @force_undeafen.error
     async def force_error(self, ctx, error):
         if isinstance(error, VoiceNotConnected):
             return await ctx.send(error)
@@ -65,6 +75,9 @@ class Force(commands.Cog):
             return await ctx.send(error)
 
         if isinstance(error, MissingPermissions):
+            return await ctx.send(error)
+
+        if isinstance(error, CommandInvokeError):
             return await ctx.send(error)
 
         traceback.print_exc()
--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,5 @@
+from discord import VoiceState
+
 from utils import Game, NotPlaying, NoGamesExist
 
 
@@ -7,3 +9,61 @@ async def get_game(games, ctx) -> Game:
             return game
         raise NotPlaying()
     raise NoGamesExist()
+
+
+async def create_game(games, ctx) -> Game:
+    voice: VoiceState = ctx.author.voice
+    game = Game(voice.channel)
+
+    # add players in voice chat to game
+    for member in voice.channel.members:
+        await member.edit(mute=False, deafen=False)
+        game.add_player(member)
+
+    # set game as started
+    game.started = True
+
+    games.append(game)
+    return game
+
+
+async def pause_game(game: Game, ctx) -> None:
+    for player in game.players:
+        await player.edit(deafen=True)
+
+    for player in game.dead_players:
+        await player.edit(mute=False)
+
+    for player in game.spectating_players:
+        await player.edit(mute=False)
+
+    await ctx.send(f"**{len(game.players)}** Living players have been deafened.\n"
+                   f"**{len(game.dead_players) + len(game.spectating_players)}"
+                   f"** Dead / Spectating players unmuted.")
+
+
+async def resume_game(game, ctx) -> None:
+    # dead and spectating players are muted
+    for player in game.dead_players:
+        await player.edit(mute=True)
+
+    for player in game.spectating_players:
+        await player.edit(mute=True)
+
+    for player in game.players:
+        await player.edit(deafen=False)
+
+    await ctx.send(f"**{len(game.players)}** players have been undeafened.\n"
+                   f"**{len(game.dead_players) + len(game.spectating_players)}"
+                   f"** Dead / Spectators players muted.")
+
+
+async def end_game(games, game):
+    for player in game.players:
+        await player.edit(mute=False, deafen=False)
+    for player in game.dead_players:
+        await player.edit(mute=False, deafen=False)
+    for player in game.spectating_players:
+        await player.edit(mute=False, deafen=False)
+
+    games.remove(game)
