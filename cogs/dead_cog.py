55ed9729131dd1cefd353b7382a69fde3c08d752
Rowan <rowanpsmith@yahoo.com.au>
2020-09-17 15:25:30 +1000

    Added so you can --dead yourself

--- cogs/dead_cog.py
+++ cogs/dead_cog.py
@@ -5,8 +5,24 @@ from discord.ext import commands
 from discord.ext.commands import Greedy
 
 from main import AmongUs
-from utils import is_playing, NoGamesExist, NotPlaying
+from utils import is_playing, NoGamesExist, NotPlaying, is_in_voice, VoiceNotConnected
 from utils.utils import get_game
+
+
+async def logic(members, game, ctx):
+    dead_count = 0
+    for player in members:
+        if game.is_alive(player):
+            game.add_dead_player(player)
+            if game.running:
+                await player.edit(mute=False, deafen=False)
+            else:
+                await player.edit(mute=True)
+        else:
+            dead_count += 1
+            await ctx.send(f"{player.name} is already dead!")
+
+    await ctx.send(f"**{len(members) - dead_count}** player(s) died!")
 
 
 class Dead(commands.Cog):
@@ -15,28 +31,25 @@ class Dead(commands.Cog):
 
     @commands.command()
     @is_playing()
+    @is_in_voice()
     async def dead(self, ctx, members: Greedy[Member] = None):
 
         game = await get_game(self.bot.games, ctx)
-        if members is None or not game.started:
+
+        if not game.started:
             return
 
-        for player in members:
-            if game.is_alive(player):
-                game.add_dead_player(player)
-                if game.running:
-                    await player.edit(mute=False, deafen=False)
-                else:
-                    await player.edit(mute=True)
-
-        await ctx.send(f"**{len(members)}** player(s) died!")
+        if members is None:
+            await logic([ctx.author], game, ctx)
+        else:
+            await logic(members, game, ctx)
 
     @dead.error
     async def dead_error(self, ctx, error):
         if isinstance(error, NoGamesExist):
             return await ctx.send(error)
 
-        if isinstance(error, NotPlaying):
+        if isinstance(error, (NotPlaying, VoiceNotConnected)):
             return await ctx.send(error)
 
         traceback.print_exc()
